654fdbbe4fcd2c2b28f7f5ea880096bd47175a8c
Doug Latornell <djl@douglatornell.ca>
2008-05-17 16:55:03 -0700

    Added 1st cut of config file interface.
    Read options and test arguments for nostests from nosy.cfg prior to
    each execution of nosetests.
    
    Did a bunch of PEP08-ification.

--- nosy.py
+++ nosy.py
@@ -1,26 +1,42 @@
-# By Jeff Winkler, http://jeffwinkler.net
-
-import glob,os,stat,time
-
-'''
+"""
 Watch for changes in all .py files. If changes, run nosetests. 
-'''
-def checkSum():
-    ''' Return a long which can be used to know if any .py files have changed.
-    Only looks in the current directory. '''
-    val = 0
-    for f in glob.glob ('*.py'):
-        stats = os.stat (f)
-        val += stats [stat.ST_SIZE] + stats [stat.ST_MTIME]
-    for f in glob.glob ('*.kid'):
-        stats = os.stat (f)
-        val += stats [stat.ST_SIZE] + stats [stat.ST_MTIME]
 
+By Jeff Winkler, http://jeffwinkler.net
+
+Command line and config file interfaces added by Doug Latornell,
+http://douglatornell.ca
+"""
+
+import glob
+import os
+import stat
+import time
+from ConfigParser import SafeConfigParser
+
+
+def checkSum():
+    """Return a long which can be used to know if any .py files have changed.
+    Only looks in the current directory.
+    """
+    val = 0
+    for f in glob.glob('*.py'):
+        stats = os.stat(f)
+        val += stats[stat.ST_SIZE] + stats[stat.ST_MTIME]
+    for f in glob.glob('tests/*.py'):
+        stats = os.stat (f)
+        val += stats[stat.ST_SIZE] + stats[stat.ST_MTIME]
     return val
 
+
 val=0
+config = SafeConfigParser()
 while (True):
     if checkSum() != val:
-        val=checkSum()
-        os.system ('nosetests')
+        val = checkSum()
+        config.readfp(open('nosy.cfg'))
+        options = config.get('nosy', 'options')
+        tests = config.get('nosy', 'tests')
+        os.system('nosetests %(options)s %(tests)s' % locals())
     time.sleep(1)
+
+# end of file
