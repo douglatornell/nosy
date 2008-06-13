84f76a404f953832c7bcc07296c59b5769bd3b59
Doug Latornell <djl@douglatornell.ca>
2008-06-12 21:18:36 -0700

    Corrected typos and got entry point syntax right.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="""\
 Run the nose test discovery and execution tool whenever a source file
 is changed.
-    """",
+    """,
     long_description="""\
 An elaborated version of Jeff Winkler's nosy tool that runs nose
 whenever a source file is changed.  This version has a command line
@@ -24,7 +24,7 @@ control what files are watched, and how nose runs.
     download_url="%(djl_url)sNosy-%(version)s.tar.gz" % locals(),
     packages=find_packages(),
     install_requires=['nose'],
-    entry_points={'consol_scripts':['nosy = Nosy.nosy:main']}
+    entry_points={'console_scripts':['nosy = nosy:main']}
     )
 
 # end of file
