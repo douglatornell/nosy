ac746416c0f78359b93b2c3c63dcd1c964df2481
Doug Latornell <djl@douglatornell.ca>
2011-09-13 19:20:54 -0700

    Add more tests.

--- tests/test_nosy.py
+++ tests/test_nosy.py
@@ -1,6 +1,7 @@
 """Unit tests for nosy.
 """
 from __future__ import absolute_import
+from mock import Mock
 import os
 from tempfile import NamedTemporaryFile
 try:
@@ -31,7 +32,7 @@ class TestNosy(unittest.TestCase):
         self.assertEqual(checksum, 0)
 
 
-    def test_extra_paths(self):
+    def test_extra_paths_checksum(self):
         """Non-zero checksum when extra_paths is not empty
         """
         nosy = self._make_one()
@@ -62,4 +63,86 @@ class TestNosy(unittest.TestCase):
         nosy = self._make_one()
         nosy.exclude_patterns = []
         exclusions = nosy._calc_exclusions('.')
-        self.assertEqual(exclusions, set())
+        self.assertItemsEqual(exclusions, set())
+
+
+    def test_exclude_patterns(self):
+        """Expected exclusions set when exclude_patterns is not empty
+        """
+        nosy = self._make_one()
+        nosy.exclude_patterns = ['*.py']
+        exclusions = nosy._calc_exclusions('./nosy')
+        self.assertItemsEqual(
+            exclusions, set(['./nosy/__init__.py', './nosy/nosy.py']))
+
+
+    def test_dir_checksum_glob_patterns_empty(self):
+        """Zero checksum when glob_patterns is empty
+        """
+        nosy = self._make_one()
+        nosy.glob_patterns = []
+        checksum = nosy._calc_dir_checksum([], '.')
+        self.assertEqual(checksum, 0)
+
+
+    def test_dir_checksum_glob_patterns_no_match(self):
+        """Zero checksum when glob pattern doesn't match
+        """
+        nosy = self._make_one()
+        nosy.glob_patterns = ['zzz*']
+        tmp_file = NamedTemporaryFile()
+        root = os.path.dirname(tmp_file.name)
+        checksum = nosy._calc_dir_checksum([], root)
+        self.assertEqual(checksum, 0)
+
+
+    def test_dir_checksum_glob_patterns_match(self):
+        """Non-zero checksum when file matching glob pattern exists in dir
+        """
+        nosy = self._make_one()
+        nosy.glob_patterns = ['tmp*']
+        tmp_file = NamedTemporaryFile()
+        root = os.path.dirname(tmp_file.name)
+        checksum = nosy._calc_dir_checksum([], root)
+        self.assertNotEqual(checksum, 0)
+
+
+    def test_dir_checksum_changes(self):
+        """Checksum changes when file is touched
+        """
+        nosy = self._make_one()
+        nosy.glob_patterns = ['tmp*']
+        tmp_file = NamedTemporaryFile()
+        root = os.path.dirname(tmp_file.name)
+        checksum1 = nosy._calc_dir_checksum([], root)
+        tmp_file.write('foobar')
+        tmp_file.flush()
+        checksum2 = nosy._calc_dir_checksum([], root)
+        self.assertNotEqual(checksum1, checksum2)
+
+
+    def test_dir_checksum_changes_when_file_excluded(self):
+        """Checksum changes when a file is excluded
+        """
+        nosy = self._make_one()
+        nosy.glob_patterns = ['tmp*']
+        tmp_file1 = NamedTemporaryFile()
+        tmp_file2 = NamedTemporaryFile()
+        root = os.path.dirname(tmp_file1.name)
+        checksum1 = nosy._calc_dir_checksum([], root)
+        checksum2 = nosy._calc_dir_checksum([tmp_file2.name], root)
+        self.assertNotEqual(checksum1, checksum2)
+
+
+    def test_checksum_method_calls(self):
+        """Number of calls & args from _checksum is as expected
+        """
+        nosy = self._make_one()
+        nosy.base_path = './nosy'
+        nosy._calc_extra_paths_checksum = Mock(return_value=0)
+        nosy._calc_exclusions = Mock(return_value=[])
+        nosy._calc_dir_checksum = Mock(return_value=0)
+        nosy._checksum()
+        nosy._calc_extra_paths_checksum.assert_called_once()
+        nosy._calc_exclusions.assert_called_once_with('./nosy')
+        nosy._calc_dir_checksum.assert_called_once_with([], './nosy')
